d2491931210ea9a1e1324d970b8481addc859c55
hereisfahad <fahadjabbar31@gmail.com>
2018-09-05 21:39:44 +0500

    configuring api call and some css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+    state = {
+        temp: '',
+        humd: '',
+        image:''
+    }
+    handleClick = async (e) => {
+        e.preventDefault();
+//        const city = e.target.elements.city.value;
+//        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&apikey=dbd802d470137cd4c005f9625b564ea2`);
+        console.log(api_call)
+        const data = await api_call.json();
+        console.log(data)
         
+      }
+  render() {
+      
+    return (
+      <div className="container">
+        <center>
+            <div className ="card" id="card1">
+                <h1>Weather</h1>
+                <form onSubmit = {this.handleClick} >
+                    <input type="text" placeholder="city" name ="city" className = "form-control"/> <br></br>
+                    <input type="text" placeholder="country" name ="country" className = "form-control"/><br></br>
+                    <button className = "btn btn-info" > Get Weather </button>
+                </form>
+            </div>
+        </center>
       </div>
     );
   }
