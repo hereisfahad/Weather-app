9d468eba3aa9ae91384d717ad1ec2b6389b746f0
hereisfahad <fahadjabbar31@gmail.com>
2018-09-05 22:29:52 +0500

    added the response data of temperature etc

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ class App extends Component {
     state = {
         temp: '',
         humd: '',
-        image:''
+        image:'',
+        loc:''
     }
     handleClick = async (e) => {
         e.preventDefault();
-//        const city = e.target.elements.city.value;
-//        const country = e.target.elements.country.value;
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&apikey=dbd802d470137cd4c005f9625b564ea2`);
-        console.log(api_call)
+//        console.log(e.target.elements.city.value)
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&apikey=dbd802d470137cd4c005f9625b564ea2`);
         const data = await api_call.json();
         console.log(data)
+        this.setState({
+            temp: data.main.temp,
+            humd: data.main.humidity,
+            image: data.weather[0].icon,
+            loc: data.name
+        })
         
       }
   render() {
@@ -29,6 +36,11 @@ class App extends Component {
                     <input type="text" placeholder="country" name ="country" className = "form-control"/><br></br>
                     <button className = "btn btn-info" > Get Weather </button>
                 </form>
+                {this.state.loc!==''?<h1>City:{this.state.loc}</h1>:''}
+                {this.state.image!==''?<img src ={this.state.image} width = "100px" height = "100px" alt = "weather icon"></img>:''}
+                {this.state.temp!==''?<h1>Humdity:{this.state.temp}</h1>:''}
+                {this.state.humd!==''?<h1>Temp:{this.state.humd}</h1>:''}
+
             </div>
         </center>
       </div>
